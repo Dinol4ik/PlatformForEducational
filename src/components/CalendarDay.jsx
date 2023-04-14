b2d2b05da5b2ce3975233402f190108ef837e3ee
9muha <d_f1@inbox.ru>
2023-04-14 15:30:56 +0500

    calendar rdy to use

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -13,11 +13,11 @@ import {
 
 const CalendarDay = (props) => {
 
-    let test6 = 0
+    let sovpadenie = 0
     const dateDay = new Date(`${props.year}.${props.month}.${props.date}`)
-let testing = new Map()
+    let testing = []
     props.lessons.map((value) => {
-        let a = new Map()
+            let a = new Map()
             const test = new Date(value.dateTime)
             const test3 = (test.toLocaleDateString('ko-KR'))
             const test5 = (test.toLocaleString('ru', {
@@ -31,19 +31,19 @@ let testing = new Map()
             a.set('subject', value.curse.subject.title)
             a.set('curse', value.curse.title)
             if (a.get('date') === dateDay.getTime()) {
-                test6 = 1
-                testing = a
+                sovpadenie = 1
+                testing.push(a)
             }
         }
     )
     return (
 
         <>
-            {test6
+            {sovpadenie
                 ?
                 <GridItem position={'relative'} minH={70} pb={2} boxShadow={'base'}
                     // bg={useColorModeValue('blackAlpha.200', 'whiteAlpha.400')}
-                            {...props}>
+                          {...props}>
                     <Box
                         as={'span'}
                         position={'absolute'} top={1} left={1}
@@ -53,23 +53,29 @@ let testing = new Map()
                     >
                         {props.date}
                     </Box>
-                    <Card marginX={3} ml={5} mt={2}>
-                        <CardHeader marginX={2} mt={1} p={0}>
-                            <Flex fontSize={'12px'} fontWeight={'500'} align={'base-line'}>
-                                <Badge colorScheme='green' mr={1}>
-                                    {testing.get('time')}
-                                </Badge>
-                                {testing.get('subject')}
 
-                            </Flex>
-                        </CardHeader>
-                        <CardBody paddingX={2} paddingY={1}>
-                            <Text fontSize='12px'>
-                                {testing.get('title')}->
-                                {testing.get('curse')}
-                            </Text>
-                        </CardBody>
+                    <Card marginX={3} ml={5} mt={2}>
+                        {React.Children.toArray(testing.map((value) => {
+                            return <><CardHeader  marginX={2} mt={1} p={0}>
+                                <Flex  fontSize={'12px'} fontWeight={'500'} align={'base-line'}>
+                                    <Badge  colorScheme='green' mr={1}>
+                                        {value.get('time')}
+                                    </Badge>
+                                    {value.get('subject')}
+
+                                </Flex>
+                            </CardHeader>
+                                <CardBody  paddingX={2} paddingY={1}>
+                                    <Text  fontSize='12px'>
+                                        {value.get('title')}->
+                                        {value.get('curse')}
+                                    </Text>
+                                </CardBody>
+                            </>
+                        }))}
+
                     </Card>
+
                 </GridItem>
                 : <GridItem position={'relative'} minH={70} pb={2} boxShadow={'base'}
                     // bg={useColorModeValue('blackAlpha.200', 'whiteAlpha.400')}

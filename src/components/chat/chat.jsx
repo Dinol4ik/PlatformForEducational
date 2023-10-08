a6f24750d664ea207c6ac4663ca217f05bf43b4d
9muha <d_f1@inbox.ru>
2023-10-08 16:02:03 +0500

    chat

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import {Button, Input} from "@chakra-ui/react";
 import SocketCon from "./ws";
-import {useParams} from "react-router-dom";
+import {json, useParams} from "react-router-dom";
 import Loader from "../Loader";
+import data from "bootstrap/js/src/dom/data";
+import login from "../login";
+
 const url = new URL(window.location.href)
 // const urlParams  = url.toString().split("/")
 // const id = urlParams[urlParams.length-1]
@@ -12,33 +15,47 @@ const url = new URL(window.location.href)
 //     return id.id
 // }
 // let connection;
+const m = []
 const Chat = () => {
     const paramsId = useParams()
     const [messages, setMessages] = useState([]);
     const id = paramsId.id
-    const [socket,setConnection] = useState()
-    if (!socket){
+    const [socket, setConnection] = useState()
+    if (!socket) {
         setConnection(SocketCon.getSocket(id))
     }
-    if (socket){
-        localStorage.setItem("chat",socket)
-        const t = typeof localStorage.getItem("chat")
-        console.log(t)
-          console.log(socket)
-     socket.onmessage = function (event) {
-        setMessages([...messages, event.data]);
-    };
-    }
+    if (socket) {
+        localStorage.setItem("chat", socket)
+        socket.onmessage = function (event) {
+            if (m.length === 0 && JSON.parse(event.data).map) {
+                // console.log(event.data, "Вначале приходит")
+                // console.log(JSON.parse(event.data), "распаршен")
+                JSON.parse(event.data).map(f => {
+                    m.push(JSON.parse(f))
+                    // setMessages([...messages, JSON.parse(f)])
+                })
+            }
+           else setMessages([...messages, JSON.parse(JSON.parse(event.data))])
 
+        };
+    }
     const handleSubmit = (event) => {
+        if (event){
         event.preventDefault();
-        const message = JSON.parse(localStorage.getItem("profileName")).first_name + ": " + event.target.elements.message.value;
-        socket.send(message);
+            }
+        // const message = JSON.parse(localStorage.getItem("profileName")).first_name + ": " + event.target.elements.message.value;
+        const message = {
+            user: JSON.parse(localStorage.getItem("profileName")).first_name,
+            message:  event? event.target.elements.message.value :"Добро пожаловать!"
+        }
+
+        socket.send(JSON.stringify(message));
         event.target.elements.message.value = '';
     };
+    console.log(messages)
     return (
         socket
-            ?<div style={{height: "1vh", textAlign: "center", marginTop: "0%"}}>
+            ? <div style={{height: "1vh", textAlign: "center", marginTop: "0%"}}>
             <pre style={{
                 position: "relative",
                 float: "left",
@@ -47,52 +64,23 @@ const Chat = () => {
                 height: "200px",
                 border: "1px solid red"
             }}>
-                {messages.map(msg => <div>
-                    {msg}
-                </div>)}
+                {/*Тут выводятся последние 50 сообщений*/}
+                {handleSubmit}
+                {m.length>0 || messages.length>0
+                    ? <>{m.map(e => <div>{e.user}:{e.message}</div>)}
+                        {messages.map(val => <div>{val.user}:{val.message}</div>)}
+                        </>
+                    :<div>Напишите сообшение чтобы вступить в чат</div>
+                }
             </pre>
 
-            <form onSubmit={handleSubmit}>
-                <Input type="text" name="message"/>
-                <Button type={"submit"}>Отправить</Button>
-            </form>
-        </div>
+                <form onSubmit={handleSubmit}>
+                    <Input type="text" name="message"/>
+                    <Button type={"submit"}>Отправить</Button>
+                </form>
+            </div>
             : <Loader/>
     );
-
-    // const [socket,setConnection] = useState(SocketCon.getSocket(id))
-    // const [messages, setMessages] = useState([]);
-    // console.log(socket)
-    // socket.onmessage = function (event) {
-    //     setMessages([...messages, event.data]);
-    // };
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const message = JSON.parse(localStorage.getItem("profileName")).first_name + ": " + event.target.elements.message.value;
-    //     socket.send(message);
-    //     event.target.elements.message.value = '';
-    // };
-    // return (
-    //     <div style={{height: "1vh", textAlign: "center", marginTop: "0%"}}>
-    //         <pre style={{
-    //             position: "relative",
-    //             float: "left",
-    //             margin: "auto",
-    //             width: "100%",
-    //             height: "200px",
-    //             border: "1px solid red"
-    //         }}>
-    //             {messages.map(msg => <div>
-    //                 {msg}
-    //             </div>)}
-    //         </pre>
-    //
-    //         <form onSubmit={handleSubmit}>
-    //             <Input type="text" name="message"/>
-    //             <Button type={"submit"}>Отправить</Button>
-    //         </form>
-    //     </div>
-    // );
 
 
 };
